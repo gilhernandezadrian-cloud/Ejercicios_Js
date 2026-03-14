63c5a2fbc5542cf025209616341e92b87387d4a9
Adrián Gil Hernández <gilhernandezadrian@fpllefia.com>
2026-03-14 12:25:01 +0100

    Ejercicio_18

---
+++ Ejercicio_18/script.js
@@ -0,0 +1,15 @@
+function dqs(sel){
+    return document.querySelector(sel)
+}
+
+const estat = dqs("#estat")
+const check = dqs("#check")
+
+// dqs("#estat").textContent
+check.addEventListener("change", function(){
+    if(check.checked){
+        estat.textContent="Actiu"
+    }else{
+        estat.textContent="Inactiu"
+    }
+})
